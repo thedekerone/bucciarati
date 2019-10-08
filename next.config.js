7427a3da69be39d098ed7ab9d5b27c7d753417c9
Mauricio Fow <mauriciofow@gmail.com>
2019-10-08 10:59:50 -0500

    initialize

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
 	// Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
 	// turn on the SW in dev mode so that we can actually test it
 	googleAnalytics   : {
+		initialize         : true,
 		parameterOverrides : {
 			cd1 : 'offline'
 		}

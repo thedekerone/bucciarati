9d49dca9c7c5abb88fcba648fb83bcb94807ed1a
Mauricio Fow <mauriciofow@gmail.com>
2019-10-08 11:12:56 -0500

    das

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
 			'/'
 		].concat(manifest), // add the homepage to the cache
 	// Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
-	// turn on the SW in dev mode so that we can actually test it
+	// turn on the SW in dev mode so that we can actually test i
 
 	workboxOpts       : {
 		swDest           : 'static/service-worker.js',

4036a7fb680668d407a6968c14c688891446692a
Mauricio Fow <mauriciofow@gmail.com>
2019-10-08 00:32:53 -0500

    xd

--- next.config.js
+++ next.config.js
@@ -46,7 +46,38 @@ const nextConfig = {
 				}
 			}
 		]
-	}
+	},
+	plugins           : [
+		// Other plugins...
+
+		new WorkboxPlugin.GenerateSW({
+			// Do not precache images
+			exclude        : [
+				/\.(?:png|jpg|jpeg|svg)$/
+			],
+
+			// Define runtime caching rules.
+			runtimeCaching : [
+				{
+					// Match any request that ends with .png, .jpg, .jpeg or .svg.
+					urlPattern : /\.(?:png|jpg|jpeg|svg)$/,
+
+					// Apply a cache-first strategy.
+					handler    : 'CacheFirst',
+
+					options    : {
+						// Use a custom cache name.
+						cacheName  : 'images',
+
+						// Only cache 10 images.
+						expiration : {
+							maxEntries : 40
+						}
+					}
+				}
+			]
+		})
+	]
 };
 
 module.exports = withOffline(nextConfig);
